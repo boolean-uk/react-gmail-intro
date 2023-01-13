113ee448f9da805235937edfbc887fa20cf2423c
Oliver West <oliversimon.west@gmail.com>
2023-01-13 16:41:46 +0000

    Completed core and extension

---
+++ src/NewEmail.js
@@ -0,0 +1,58 @@
+import './styles/newEmail.css'
+
+function NewEmail() {
+    return (
+        <div className="newEmailWrap">
+                <div className="profileWrapper"></div>
+                <div className="sendEmailWrapper">
+                    <div className="newEmailHeader">
+                        <div className="replyarrow"></div>
+                        <div className="emailaddress">Freepik Company (no-reply@freepik.com)</div>
+                        <div className="popouticon"></div>
+                    </div>
+                    <div className="newEmailMain">
+                        <div className="threedots"></div>
+                    </div>
+                    <div className="newEmailFooter">
+                        <div className="sendButton"></div>
+                        <div className="newEmailIcons">
+                            <div className="fontIcon">
+
+                            </div>
+                            <div className="attachIcon">
+
+                            </div>
+                            <div className="linkIcon">
+
+                            </div>
+                            <div className="emojisIcon">
+
+                            </div>
+                            <div className="gdriveIcon">
+
+                            </div>
+                            <div className="picturesIcon">
+
+                            </div>
+                            <div className="timeLockIcon">
+
+                            </div>
+                            <div className="markupIcon">
+
+                            </div>
+                        </div>
+                        <div className="deleteAndMore">
+                            <div className="burgerBar">
+
+                            </div>
+                            <div className="trashCan">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+};
+
+export default NewEmail
