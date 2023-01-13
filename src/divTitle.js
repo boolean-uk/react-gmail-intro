f5c744aec56ffe4fe45b1c6e91facd0b8597004c
Hamza Ahmed <hamza.anwar@outlook.com>
2023-01-13 19:43:46 +0000

    done

---
+++ src/divTitle.js
@@ -0,0 +1,9 @@
+import "./styles/title.css"
+function DivTitle () {
+    return (
+        <div className="title">
+        <h1>Welcome to Flaticon</h1>
+      </div>
+    )
+}
+export default DivTitle
