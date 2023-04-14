28a5d4f0022b78ef77cb93aae79825ff18177b7d
Konstantinos Vrs <it21712@hua.gr>
2023-04-14 14:55:50 +0300

    added EmailWrite component

--- src/components/email_view/EmailView.js
+++ src/components/email_view/EmailView.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import '../../styles/EmailView.css'
-import flaticonWelcomeImage from '../../assets/images/flaticon-welcome-image.png'
-import backArrow from '../../assets/icons/back-arrow.png'
 
-import rateStarButton from '../../assets/icons/rate-star-button.png'
-import rubbishButton from '../../assets/icons/rubbish-bin-delete-button.png'
 import EmailToolbar from './EmailToolbar'
 import EmailContent from './EmailContent'
+import EmailWrite from './EmailWrite'
 
 const EmailView = () => {
 
@@ -14,6 +11,7 @@ const EmailView = () => {
     <main className="email-view">
       <EmailToolbar/>
       <EmailContent />
+      <EmailWrite/>
     </main>
   )
 
---
+++ src/components/email_view/EmailWrite.js
@@ -0,0 +1,39 @@
+import '../../styles/EmailWrite.css'
+import unknown from '../../assets/images/unknown.webp'
+import reply_arrow from '../../assets/images/reply_arrow.png'
+import { ChevronDownIcon, PaperClipIcon, PhotoIcon } from '@heroicons/react/20/solid'
+import {FaceSmileIcon} from '@heroicons/react/24/outline'
+const EmailWrite = () => {
+    return (
+        <form className="email-write-row">
+            <img className='avatar' src={unknown}></img>
+            <article className='email-write-content'>
+                <div className='write-header'>
+                    <img className='reply-icon' src={reply_arrow}></img>
+                    <ChevronDownIcon className='icon' color='gray' />
+                    <div className='to-container'>
+                        <h3 className='header-to'>Freepik Company (no-reply@freepik.com)</h3>
+                    </div>
+                </div>
+                <textarea className='content-input'></textarea>
+                <div className='dots'>···
+                </div>
+                <div className='write-actions'>
+                    <button className='send-button' type='submit'>
+                        <div className='send-button-a'>
+                            <h3>Send</h3>
+                        </div>
+                        <div className='send-button-b'>
+                            <ChevronDownIcon className='icon' color='white'/>
+                        </div>
+                    </button>
+                    <PaperClipIcon className='icon' color='gray'/>
+                    <PhotoIcon className='icon' color='gray'/>
+                    <FaceSmileIcon className='icon' color='gray'/>
+                </div>
+            </article>
+        </form>
+    )
+}
+
+export default EmailWrite
