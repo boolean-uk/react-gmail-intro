38262172c6e0b6a6fd7a9ef19eecb8b047082b97
Iasonas Kotsarapoglou <kotsarapoglouiasonas@gmail.com>
2023-04-18 01:42:52 +0300

    Component Folder Added

---
+++ src/components/main/index.js
@@ -0,0 +1,11 @@
+import Content from "../content/index.js"
+import ToolBar from "../toolBar/index.js"
+function Main() {
+    return (
+        <div>
+            <ToolBar />
+            <Content />
+        </div>
+    )
+}
+export default Main
