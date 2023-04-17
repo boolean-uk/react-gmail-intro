6f264db894b9005b12b15d84299e7bfad8cf2573
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-04-17 19:54:14 +0300

    extension - cheat used

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,11 +1,15 @@
-import EmailToolbar from './emailToolbar/emailToolbar'
+import EmailToolbar from './email-toolbar/emailToolbar'
 import EmailContent from './emailContent/emailContent'
+import ActionButtons from './action-buttons/actionButtons'
+import QuickReply from './quick-reply/quickReply'
 
 function Main() {
   return (
     <main className="email-view">
       <EmailToolbar />
       <EmailContent />
+      <ActionButtons />
+      <QuickReply />
     </main>
   )
 }
