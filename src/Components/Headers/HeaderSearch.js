aed89c56ac6b68af353bf5dbdec711222ff134ea
Rose Breedveld <rosebreedveld@hotmail.com>
2023-04-17 14:21:24 -0400

    standard react gmail intro

---
+++ src/Components/Headers/HeaderSearch.js
@@ -0,0 +1,11 @@
+function HeaderSearch() {
+    return (
+        <header>
+        <div className="search">
+          <input className="search-bar" placeholder="Search mail" />
+        </div>
+      </header>
+    )
+}
+
+export default HeaderSearch
