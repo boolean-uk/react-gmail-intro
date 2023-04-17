a3c38c8dfe105fb1a66d3e5f765df18cde415e5f
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-04-17 15:54:10 +0300

    jsconfig.json created for easier imports

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import './styles/app.css'
 
-import flaticonWelcomeImage from './assets/images/flaticon-welcome-image.png'
-import backArrow from './assets/icons/back-arrow.png'
-import downloadButton from './assets/icons/download-button.png'
-import rateStarButton from './assets/icons/rate-star-button.png'
-import rubbishButton from './assets/icons/rubbish-bin-delete-button.png'
+import flaticonWelcomeImage from 'assets/images/flaticon-welcome-image.png'
+import backArrow from 'assets/icons/back-arrow.png'
+import downloadButton from 'assets/icons/download-button.png'
+import rateStarButton from 'assets/icons/rate-star-button.png'
+import rubbishButton from 'assets/icons/rubbish-bin-delete-button.png'
 
 function App() {
   return (
