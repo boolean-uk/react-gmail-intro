618c12f7f6ae5ef2eb9e306b72800a9571477925
Todd Griffin <todd.griffin61@outlook.com>
2023-04-27 17:28:54 +0100

    finished core

---
+++ src/write-email.js
@@ -0,0 +1,35 @@
+import './styles/write-email.css'
+
+export default function WriteEmail() {
+  return(
+    <div className="compose-email">
+      <img></img>
+      <div>
+        <div className="compose-title">
+          <img src="" className="compose-reply"></img>
+          <img src="" className="compose-dropdown"></img>
+          <div className="compose-recipient"></div>
+        </div>
+        <div className="compose-body">
+          <p></p>
+        </div>
+        <img src='' className='compose-three-dots'></img>
+        <div className="compose-icons">
+          <div className='compose-send-button'></div>
+          <img src=""></img>
+          <img src=""></img>
+          <img src=""></img>
+          <img src=""></img>
+          <img src=""></img>
+          <img src=""></img>
+          <img src=""></img>
+          <img src=""></img>
+          <div></div>
+          <img src=""></img>
+          <img src=""></img>
+        </div>
+      </div>
+
+    </div>
+  )  
+}
---
+++ src/write-email.jsx
@@ -0,0 +1,14 @@
+export default function WriteEmail() {
+  return(
+    <div className="compose-email">
+      <img></img>
+      <div>
+        <div className="title"></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+    </div>
+  )  
+}
