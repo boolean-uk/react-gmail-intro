98d8f15f5699fa9b90fa2b2e61266a99382cd9b9
Annefleur Hay <annefleurhay1@gmail.com>
2023-08-16 13:55:45 +0200

    header js added

---
+++ src/components/header/HeaderSearchBar.js
@@ -0,0 +1,7 @@
+function HeaderSearchBar(){
+    return (<div className="search">
+    <input className="search-bar" placeholder="Search mail" />
+  </div>)
+}
+
+export default HeaderSearchBar
---
+++ src/components/header/Menu.js
@@ -0,0 +1,11 @@
+import HamburgerMenu from "./HamburgerMenu"
+import HeaderSearchBar from "./HeaderSearchBar"
+function Menu () {
+    return (<header className="header">
+    <HamburgerMenu/>
+    <HeaderSearchBar/>
+    
+  </header>)
+}
+
+export default Menu
