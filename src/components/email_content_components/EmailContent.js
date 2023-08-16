309a49dd6c466719980946cd1279f48ddfff90cd
ks46 <konastaf46@gmail.com>
2023-08-16 14:56:38 +0300

    Created EmailContent component and subcomponents

---
+++ src/components/email_content_components/EmailContent.js
@@ -0,0 +1,19 @@
+import '../../styles/app.css'
+
+import EmailTitle from './EmailTitle'
+import EmailHeader from './EmailHeader'
+import EmailBody from './EmailBody'
+import EmailActions from './EmailActions'
+
+function EmailContent() {
+  return (
+    <article className="email-content">
+      <EmailTitle />
+      <EmailHeader />
+      <EmailBody />
+      <EmailActions />
+    </article>
+  )
+}
+
+export default EmailContent
