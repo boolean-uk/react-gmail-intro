2f5c45f4a60e3542b7c86a76442f1b0faa8f64ef
ks46 <konastaf46@gmail.com>
2023-08-17 02:12:28 +0300

    Created ComposeArea component

--- src/components/email_compose_components/ComposeArea.js
+++ src/components/email_compose_components/ComposeArea.js
@@ -2,9 +2,7 @@ import '../../styles/email_compose.css'
 
 function ComposeArea() {
   return (
-    <div className="compose-area">
-      <p>Area for writing the email</p>
-    </div>
+    <div className="compose-area"></div>
   )
 }
 
