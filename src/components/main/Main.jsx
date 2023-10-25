0f1326229c8329ac23f7381dfcda05ca98823b85
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-10-25 17:44:37 +0100

    hap-hazardly added elements to email-reply

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,13 @@
 import EmailToolbar from "./EmailToolbar";
 import EmailContent from "./EmailContent/EmailContent";
+import EmailReply from "./EmailReply";
 
 function Main() {
   return (
     <main className="email-view">
       <EmailToolbar></EmailToolbar>
       <EmailContent></EmailContent>
+      <EmailReply></EmailReply>
     </main>
   );
 }
