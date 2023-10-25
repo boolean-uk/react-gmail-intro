58d8221fd351730a2f5a67ee5dde1552e193ed06
TomEastwood <t.eastwood99@gmail.com>
2023-10-25 17:24:27 +0100

    extensions complete (pretty barebones but works)

---
+++ src/components/Main/Email-View/Write-Email/Write-Email.jsx
@@ -0,0 +1,14 @@
+import '/src/styles/write-emails.css'
+
+function WriteEmail () {
+    return (
+        <>
+        <div className="text-box">
+            <p className="placeholder-text">noansondokn@gmail.com</p>
+            <button className="send-button">Send</button>
+        </div>
+        </>
+    )
+}
+
+export default WriteEmail
--- src/components/Main/MainBody.jsx
+++ src/components/Main/MainBody.jsx
@@ -4,6 +4,7 @@ import Info from './Email-View/Email-Content/Content-Header/Info'
 import ActionIcons from './Email-View/Email-Content/Content-Header/Action-Icons'
 import EmailBody from './Email-View/Email-Body/EmailBody'
 import EmailActions from './Email-View/Email-Actions/EmailActions'
+import WriteEmail from './Email-View/Write-Email/Write-Email'
 import '/src/styles/email-content/content-header.css'
 
 function MainBody() {
@@ -23,6 +24,7 @@ function MainBody() {
           <EmailBody />
 
           <EmailActions />
+          <WriteEmail />
         </article>
       </main>
       </>
