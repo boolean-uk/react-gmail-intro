75aafb06f3ad6c69a1b635f4ef9716916954894c
Satoki <satokiito@gmail.com>
2023-10-25 15:06:54 +0100

    Header broken down into sub-components

---
+++ src/components/jsx-components/HeaderSearch.jsx
@@ -0,0 +1,10 @@
+function HeaderSearch() {
+
+    return (
+        <div className="search">
+                <input className="search-bar" placeholder="Search mail" />
+        </div>
+    )
+}
+
+export default HeaderSearch
