f09ef97fc0644b96a74ab5b55bad447bf9d4ed8d
Satoki <satokiito@gmail.com>
2023-10-25 14:55:41 +0100

    styling extracted into separate components

--- src/components/jsx-components/Main.jsx
+++ src/components/jsx-components/Main.jsx
@@ -4,6 +4,8 @@ import downloadButton from '../../assets/icons/download-button.png'
 import rateStarButton from '../../assets/icons/rate-star-button.png'
 import rubbishButton from '../../assets/icons/rubbish-bin-delete-button.png'
 
+import '../css-components/Main.css'
+
 function Main() {
 
     return (
--- src/components/jsx-components/Nav.jsx
+++ src/components/jsx-components/Nav.jsx
@@ -1,3 +1,5 @@
+import '../css-components/Nav.css'
+
 function Nav() {
 
     return (
