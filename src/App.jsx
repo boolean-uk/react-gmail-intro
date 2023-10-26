ab528912657a1f6fc0fe359d2fb1e03fede4f388
Mobeen <muhammadmobeen1@hotmail.com>
2023-10-26 16:40:28 +0200

    Done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './styles/App.css'
-import Menu from './Components/Menu'
-import Header from './Components/Header'
-import Main from './Components/Main'
+import Menu from './Components/Menu.jsx'
+import Header from './Components/Header.jsx'
+import Main from './Components/Main.jsx'
 function App() {
   return (
     <div className="app">
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,9 @@
 import './Main.css'
-
+import flaticonWelcomeImage from '../assets/images/flaticon-welcome-image.png'
+import backArrow from '../assets/icons/back-arrow.png'
+import downloadButton from '../assets/icons/download-button.png'
+import rateStarButton from '../assets/icons/rate-star-button.png'
+import rubbishButton from '../assets/icons/rubbish-bin-delete-button.png'
 function Main () {
     return (
         <main className="email-view">
