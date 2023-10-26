829c47fecca447dbebcf939f6f11608f70cf8a58
Rebecca Noy <rebecca.noy@gmx.de>
2023-10-26 08:43:26 +0200

    got Navtoolbar to work yay

--- src/App.jsx
+++ src/App.jsx
@@ -2,66 +2,21 @@ import './styles/App.css'
 
 import flaticonWelcomeImage from './assets/images/flaticon-welcome-image.png'
 import backArrow from './assets/icons/back-arrow.png'
-import downloadButton from './assets/icons/download-button.png'
+
 import rateStarButton from './assets/icons/rate-star-button.png'
 import rubbishButton from './assets/icons/rubbish-bin-delete-button.png'
+
+import LeftMenu from './components/header/leftMenu';
+import Header from "./components/header/header";
+import NavToolbar from "./components/main/navToolbar";
 
 function App() {
   return (
     <div className="app">
-      <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
-      </header>
-      <nav className="left-menu">
-        <ul className="inbox-list">
-          <li className="item active">
-            <span className="label">Inbox</span>
-            <span className="count">3</span>
-          </li>
-          <li className="item">
-            <span className="label">Starred</span>
-            <span className="count">2</span>
-          </li>
-
-          <li className="item toggle">
-            <label htmlFor="hide-read">Hide read</label>
-            <input id="hide-read" type="checkbox" checked={false} />
-          </li>
-        </ul>
-      </nav>
+      <Header></Header>
+      <LeftMenu></LeftMenu>
       <main className="email-view">
-        <nav className="email-toolbar">
-          <ul>
-            <li>
-              <img className="icon" src={backArrow} alt="reply button" />
-            </li>
-            <li>
-              <img className="icon" src={downloadButton} alt="archive button" />
-            </li>
-            <li>
-              <img className="icon" src={rubbishButton} alt="delete button" />
-            </li>
-          </ul>
-          <div className="space"></div>
-          <div>
-            <p>1 of 25</p>
-            <button>&lt;</button>
-            <button>&gt;</button>
-          </div>
-        </nav>
+        <NavToolbar></NavToolbar>
         <article className="email-content">
           <div className="title">
             <h1>Welcome to Flaticon</h1>
---
+++ src/components/main/navToolbar/listItem.jsx
@@ -0,0 +1,9 @@
+function ListItem() {
+  return (
+    <li>
+          <img className="icon" src="{name}" alt="reply button" />
+    </li>
+  )
+}
+
+export default ListItem
