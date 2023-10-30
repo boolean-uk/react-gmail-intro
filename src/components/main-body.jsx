3ec90c2731362c6111dbfa789448fcfe2834e664
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-10-30 10:38:51 +0000

    components seperated (react only)

--- src/components/main-body.jsx
+++ src/components/main-body.jsx
@@ -1,7 +1,26 @@
-import"./styles/seperated-styles/main-body.css"
-
+import './styles/seperated-styles/main-body.css'
 import flaticonWelcomeImage from './assets/images/flaticon-welcome-image.png';
 
-function main() {
-  return()
+function MainBody() {
+  return (
+    <main className="email-view">
+      <article className="email-content">
+        <div className="title">
+          <h1>Welcome to Flaticon</h1>
+        </div>
+        {/* ... */}
+        <section className="email-body">
+          <div className="content">
+            <img src={flaticonWelcomeImage} alt="Flaticon welcome message" />
+          </div>
+        </section>
+        <section className="email-actions">
+          <button>Reply</button>
+          <button>Forward</button>
+        </section>
+      </article>
+    </main>
+  );
 }
+
+export default MainBody;
