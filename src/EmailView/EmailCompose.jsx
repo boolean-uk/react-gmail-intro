e1ccf9a1d69e6ade78a555ae8f401edd7ad95c94
Mårten Elfving <marten.elving@gmail.com>
2024-02-21 16:10:10 +0100

    Core+ext

---
+++ src/EmailView/EmailCompose.jsx
@@ -0,0 +1,16 @@
+import "./EmailCompose.css";
+
+export default function EmailCompose() {
+  return (
+    <div className="BoundingBox">
+      <div className="Inside">
+        <div className="row Top-Row">1</div>
+        <div className="row Email-Field"></div>
+        <div className="row Bottom-Row">
+          <button className="button">Send mail</button>
+          <p>2</p>
+        </div>
+      </div>
+    </div>
+  );
+}
