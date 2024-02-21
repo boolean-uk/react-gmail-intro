eaabd86819971a589e553272ad16dfbaf23204b4
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-02-21 14:32:11 +0100

    refined extension

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,8 +1,6 @@
-import rateStarButton from "./assets/icons/rate-star-button.png";
 import flaticonWelcomeImage from "./assets/images/flaticon-welcome-image.png";
-import backArrow from "./assets/icons/back-arrow.png";
-import rubbishButton from "./assets/icons/rubbish-bin-delete-button.png";
 import WriteEmailImage from "./assets/images/write-email-component.png";
+import ArticleHeader from "./ArticleHeader";
 
 function Article() {
   return (
@@ -10,36 +8,7 @@ function Article() {
       <div className="title">
         <h1>Welcome to Flaticon</h1>
       </div>
-      <header className="email-content--header">
-        <div className="avatar"></div>
-        <div className="email-info">
-          <div className="sender-info">
-            <h2>Freepik Company</h2>
-            <em>&lt;no-reply@freepik.com&gt;</em>
-          </div>
-          <div className="user-info">
-            <p>
-              to me <em>&lt;nicolas@boolean.co.uk&gt;</em>
-            </p>
-          </div>
-        </div>
-        <div className="date-info">
-          <p>17 March 2021, 09:33</p>
-        </div>
-        <div className="email-action-icons">
-          <ul>
-            <li>
-              <img className="icon" src={backArrow} alt="reply button" />
-            </li>
-            <li>
-              <img className="icon" src={rateStarButton} alt="star button" />
-            </li>
-            <li>
-              <img className="icon" src={rubbishButton} alt="delete button" />
-            </li>
-          </ul>
-        </div>
-      </header>
+      <ArticleHeader />
       <section className="email-body">
         <div className="content">
           <img src={flaticonWelcomeImage} alt="Flaticon welcome message" />
@@ -54,7 +23,6 @@ function Article() {
           src={WriteEmailImage}
           alt="Write Email"
           style={{ width: "1000px", height: "300px" }}
-          //onClick={handleWriteEmail}
         />
       </section>
     </article>
