65342fd99b3edb0fb4c4f5b4c91c8402fca6e54b
Sini Suihkonen <sahsuihkonen@gmail.com>
2024-02-21 15:16:55 +0200

    core + extension

---
+++ src/SearchBar.jsx
@@ -0,0 +1,11 @@
+import "./styles/SearchBar.css";
+
+function SearcBar() {
+  return (
+    <div className="search">
+      <input className="search-bar" placeholder="Search mail" />
+    </div>
+  );
+}
+
+export default SearcBar;
