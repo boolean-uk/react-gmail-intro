3bbe9424a0e4663ee19c3ad6f6485469e5621e14
spectraldesign <spectraldesign1@gmail.com>
2024-02-22 10:21:48 +0100

    Core + Extension - Might style extension more later

---
+++ src/components/WriteEmailActionBar.jsx
@@ -0,0 +1,14 @@
+import '../styles/WriteEmailActionBar.css';
+
+export default function WriteEmailActionBar() {
+    return (
+        <div className="write-email-action-bar">
+            <button className='send-email-button'>Send</button>
+            <button>Style</button>
+            <button>Attachment</button>
+            <button>Link</button>
+            <button>Emoji</button>
+            <button>Drive</button>
+        </div>
+    )
+}
