97f04d60093e5e9a7f401f70a7f8387c7e316adf
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-22 12:56:27 +0100

    Extension - Made compose email interactive (no sending functionality though)

--- src/Components/ComposeEmail/ComposeEmail.jsx
+++ src/Components/ComposeEmail/ComposeEmail.jsx
@@ -1,12 +1,14 @@
 import ComposeEmailFooter from './ComposeEmailFooter/ComposeEmailFooter'
 import ComposeEmailHeader from "./ComposeEmailHeader/ComposeEmailHeader"
+import ComposeEmailText from "./ComposeEmailText/ComposeEmailText"
 import "./ComposeEmail.css"
 
 const ComposeEmail = () => {
     return (
         <div className="email-composer">
             <div className="compose-area">
-                <ComposeEmailHeader name="Freepik Company" email="no-reply@freepik.com"/>
+                <ComposeEmailHeader/>
+                <ComposeEmailText/>
                 <ComposeEmailFooter/>
             </div>
         </div>
--- src/Components/ComposeEmail/ComposeEmailHeader/ComposeEmailHeader.jsx
+++ src/Components/ComposeEmail/ComposeEmailHeader/ComposeEmailHeader.jsx
@@ -1,19 +1,43 @@
 import { useState } from 'react'
+import "./ComposeEmailHeader.css"
 import PropTypes from 'prop-types'; // Required by boolean es lint for prop type validation
 import backArrow from '@/assets/icons/back-arrow.png'
 import Down from "@/assets/icons/NewIcons/triangle-bottom-arrow-icon.png"
 
-const ComposeEmailHeader = (props) => {
+const ComposeEmailHeader = () => {
+    const [showSpan, setShowSpan] = useState(true)
     // eslint-disable-next-line no-unused-vars
-    const [recipient, setRecipient] = useState(props.name)
+    const [recipient, setRecipient] = useState("Freepik")
     // eslint-disable-next-line no-unused-vars
-    const [recipientEmail, setRecipientEmail] = useState(props.email)
+    const [recipientEmail, setRecipientEmail] = useState("no-replay@freepik.com")
+
+    const toggleSpan = () => {
+        if (showSpan) {
+            setShowSpan(false)
+        } else {
+            setShowSpan(true)
+        }
+    }
+
+    const submitEmail = (e) => {
+        if (e.key === "Enter") {
+            const targetEmail = e.target.value
+            setRecipientEmail(targetEmail)
+            let recipientName = targetEmail.substring(0, targetEmail.indexOf("@"))
+            recipientName = recipientName.substring(0,1).toUpperCase() + recipientName.slice(1)
+            setRecipient(recipientName)
+            setShowSpan(true)
+        }
+    }
 
     return (
         <div className="compose-header"> 
             <img src={backArrow} alt="reply button" />
             <img style={{height: "5px"}}src={Down}></img>
-            <span>{recipient} ({recipientEmail})</span>                  
+            {showSpan ? 
+                <span onClick={() => toggleSpan()}>{recipient} ({recipientEmail})</span> : 
+                <input placeholder="value" onKeyDown={(e) => submitEmail(e)}/>
+            }       
         </div>
     )
 }
