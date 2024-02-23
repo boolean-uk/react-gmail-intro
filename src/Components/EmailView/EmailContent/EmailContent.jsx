4755466062def88ab4b0a39eb8ad5879dd5f6a1d
kristianverduin <kristianverduin@gmail.com>
2024-02-23 10:40:23 +0100

    Finished extension

--- src/Components/EmailView/EmailContent/EmailContent.jsx
+++ src/Components/EmailView/EmailContent/EmailContent.jsx
@@ -2,6 +2,7 @@ import Title from './Title/Title'
 import Header from './Header/Header'
 import Body from './Body/Body'
 import Actions from './Actions/Actions'
+import Reply from './Reply/Reply'
 
 function EmailContent() {
   return (
@@ -10,6 +11,7 @@ function EmailContent() {
       <Header />
       <Body />
       <Actions />
+      <Reply />
     </article>
   )
 }
