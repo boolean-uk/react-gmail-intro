325a7a833a7bcddb1bac9ec599a4664e7988cfe4
msg <mats.gravem@no.experis.com>
2024-02-23 13:10:02 +0100

    Core

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
+export { default as MainView } from './MailView';
+
+export { default as EmailContent } from './mail/EmailContent'
