2c93532f6edbe630ec0417452cf76533d096e904
Mikolaj <mkmbaran@gmail.com>
2024-02-23 10:52:32 +0100

    Core done

---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,10 @@
+import MainToolbar from "./Main/MainToolbar"
+import MainContent from "./Main/MainContent"
+
+function MainPage () {
+    return (<main className="email-view">
+    <MainToolbar/>
+    <MainContent/>
+  </main>)
+}
+export default MainPage
