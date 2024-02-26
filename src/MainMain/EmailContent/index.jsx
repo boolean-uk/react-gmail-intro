3b212b319655fe9ad4b2c5dcd11dbeccd032da89
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-02-26 14:49:40 +0100

    added extension

--- src/MainMain/EmailContent/index.jsx
+++ src/MainMain/EmailContent/index.jsx
@@ -4,27 +4,31 @@ import EmailBody from './EmailBody'
 import EmailActions from './EmailActions'
 
 function EmailContent() {
-    return (
-        <article className="email-content">
+  return (
+    <article className="email-content">
 
-        <div className="title">
-          <h1>Welcome to Flaticon</h1>
-        </div>
+      <div className="title">
+        <h1>Welcome to Flaticon</h1>
+      </div>
 
-        <EmailContentHeader/>
-        
-        <EmailBody/>
-        
-        <EmailActions/>
+      <EmailContentHeader />
 
-        {'<'}!-- Extension bit --{'>'}
-        <br />
+      <EmailBody />
+
+      <EmailActions />
+
+
+      <div className='extension'>
+        <img src="../../../images/usericon.png" alt="user icon" />
         <textarea className="new-email-input"></textarea>
-        <br />
-        <button>Send</button>
+        <div className='bottom-panel'>
+          <button>Send</button>
+          <img className='littleIcons' src='../../../images/littleicons.png' alt='little icons'></img>
+        </div>
+      </div>
 
-      </article>
-    )
+    </article>
+  )
 }
 
 export default EmailContent
