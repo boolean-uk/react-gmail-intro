9201415cc35af86b22e6fb0fa35d663b0f6e9d1f
Sebastian <sebastian.hanssen@outlook.com>
2024-02-26 15:18:01 +0100

    finish exercise

---
+++ src/EmailInput.jsx
@@ -0,0 +1,11 @@
+import './styles/EmailInput.css'
+
+function EmailInput() {
+  return (
+    <div className="email">
+      <textarea className="email-bar" placeholder="Reply to no-reply@freepik.com" />
+    </div>
+  )
+}
+
+export default EmailInput
